a58879d6fab6447e5ad5807fae5b85bc3dc28e13
mahabur-rahman21 <mahabursoikot04@gmail.com>
2025-03-01 16:35:18 +0600

    all ui task done

---
+++ js/blog.js
@@ -0,0 +1,4 @@
+let backToDesk = document.getElementById("backToDesk");
+backToDesk.addEventListener("click", function (e) {
+  window.location.href = "index.html";
+});
---
+++ js/main.js
@@ -0,0 +1,8 @@
+let btnDiscover=document.getElementById("btnDiscover")
+
+
+
+
+btnDiscover.addEventListener("click",function(e){
+    window.location.href="./blog.html"
+})
